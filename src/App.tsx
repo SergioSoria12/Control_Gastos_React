74758d23c017b918f9e2e527b624b060e06336cb
SergioSoria12 <sergio.soria.secadras@gmail.com>
2025-09-16 15:16:38 +0200

    Empezar a crear app y añadir la logica

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
+import BudgetForm from "./components/BudgetForm"
+import BudgetTracker from "./components/BudgetTracker";
+import ExpenseModal from "./components/ExpenseModal";
+import { useBudget } from "./hooks/useBudget"
+import { useMemo } from "react";
 
 function App() {
 
+  const { state } = useBudget()
+  
+  const isValidBudget = useMemo(() => state.budget > 0, [state.budget])
+  
+
   return (
     <>
-      <h1>Control de Gastos</h1>
+      <header className="bg-blue-600 py-8 max-h-72">
+        <h1 className="uppercase text-center font-black text-4xl text-white">
+          Planificador de Gastos
+        </h1>
+      </header>
+
+      <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
+        {isValidBudget ? <BudgetTracker/> : <BudgetForm/>}
+      </div>
+
+      {isValidBudget && (
+        <main className="max-w-3xl mx-auto py-10">
+          
+           <ExpenseModal/>
+        </main>
+       
+      )}
     </>
   )
 }
---
+++ src/components/ExpenseForm.tsx
@@ -0,0 +1,141 @@
+import { useState, type ChangeEvent, type FormEvent } from "react";
+import { categories } from "../data/categories";
+import DatePicker from 'react-date-picker';
+import 'react-date-picker/dist/DatePicker.css';
+import 'react-calendar/dist/Calendar.css';
+import type { DraftExpense, Value } from "../types";
+import ErrorMessage from "./ErrorMessage";
+import { useBudget } from "../hooks/useBudget";
+
+
+
+export default function ExpenseForm() {
+
+    const [expense, setExpense] = useState<DraftExpense>({
+        amount: 0,
+        expenseName: '',
+        category: '',
+        date: new Date()
+    })
+
+    const [error, setError] = useState('')
+    const { dispatch } = useBudget()
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
+        const { name, value } = e.target
+        const isAmountField = ['amount'].includes(name)
+        setExpense({
+            ...expense,
+            [name] : isAmountField ? +value : value
+        })
+    }
+
+    const handleChangeDate = (value: Value) => {
+        setExpense({
+            ...expense,
+            date: value
+        })
+    }
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        // Validar todos los campos
+        if (
+            expense.expenseName.trim() === '' ||
+            expense.amount <= 0 ||
+            expense.category === '' ||
+            !expense.date
+        ) {
+            setError('Todos los campos son obligatorios')
+            return
+        }
+
+        setError('')
+        // Agregar un nuevo gasto
+        dispatch({type: 'add-expense', payload: {expense}})
+    }
+
+
+  return (
+    <form className="space-y-5" onSubmit={handleSubmit}>
+        <legend
+            className="uppercase text-center text-2xl font-black border-b-4 border-blue-500 py-2"
+        >Nuevo Gasto</legend>
+
+        {error && <ErrorMessage>{error}</ErrorMessage>}
+
+        <div className="flex flex-col gap-2">
+            <label 
+                htmlFor="expenseName"
+                className="text-xl"    
+            >Nombre del Gasto:</label>
+            <input 
+                type="text"
+                id="expenseName"
+                placeholder="Añade el Nombre del gasto"
+                className="bg-slate-100 p-2"
+                name="expenseName"
+                onChange={handleChange}
+            />
+        </div>
+        
+        <div className="flex flex-col gap-2">
+            <label 
+                htmlFor="amount"
+                className="text-xl"    
+            >Cantidad:</label>
+            <input 
+                type="number"
+                id="amount"
+                placeholder="Añade la cantidad del gasto: Ej. 300"
+                className="bg-slate-100 p-2"
+                name="amount"
+                onChange={handleChange}
+            />
+        </div>
+
+        <div className="flex flex-col gap-2">
+            <label 
+                htmlFor="category"
+                className="text-xl"    
+            >Categoría:</label>
+            <select 
+                id="category"
+                className="bg-slate-100 p-2"
+                name="category"
+                onChange={handleChange}
+            >
+                <option value="">-- Seleccione --</option>
+                {categories.map( category => (
+                    <option
+                        id={category.id}
+                        value={category.id}
+                    >
+                        {category.name}
+                    </option>
+                ))}
+            </select>
+        </div>
+
+         <div className="flex flex-col gap-2">
+            <label 
+                htmlFor="amount"
+                className="text-xl"    
+            >Fecha Gasto:</label>
+            <DatePicker
+                className="bg-slate-100 p-2 border-0"
+                value={expense.date}
+                onChange={handleChangeDate}
+            />
+        </div>
+
+        <input 
+            type="submit" 
+            className="bg-blue-600 cursor-pointer w-full p-2 text-white uppercase
+            font-bold rounded-lg"
+            value={'Registrar Gasto'}
+        />
+    </form>
+  )
+}
